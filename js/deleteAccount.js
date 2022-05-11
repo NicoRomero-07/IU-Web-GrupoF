54b9638788c8c16ca4a17c42e82d7b0fafc6df46
pablogodiaz <pablogodiaz+1@gmail.com>
2022-05-11 10:21:57 +0200

    Fixed "Borrar Cuenta" button
    Added mouseenter and mouseover events, and added bootstrap modal functionality.

---
+++ js/deleteAccount.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("deleteaccountbtn");
+
+button.addEventListener("mouseenter", function(event) {
+
+    button.classList.remove('text-danger');
+    button.classList.add('text-white');
+
+  }, false);
+
+button.addEventListener("mouseout", function(event) {
+
+    button.classList.remove('text-white');
+    button.classList.add('text-danger');
+
+}, false);
