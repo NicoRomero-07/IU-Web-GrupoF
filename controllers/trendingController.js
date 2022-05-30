31c893dc23d72bcd9198616eca31bcfc780abef8
nicoromero <nicoromero07@hotmail.com>
2022-05-30 19:49:02 +0200

    añadido a trending topAutoreas

--- controllers/trendingController.js
+++ controllers/trendingController.js
@@ -5,25 +5,37 @@ const mysql = db.mysql;
 // Controlador del apartado trending
 exports.trending = async(req, res)=>{
     if(typeof req.session.loggedin != "undefined"){
-        let selectQuery = 'SELECT f.idForo,f.propietario,f.nombre,f.descripcion,count(m.idMensaje_foro) mensajes FROM bocaillo.foro f' + 
-        ' left join mensaje_foro m ON f.idForo = m.foro group by f.idForo ORDER BY COUNT(m.idMensaje_foro) DESC';
-        let query = mysql.format(selectQuery,["foro"]);
-        pool.query(query,(err,data) => {
+        let selectQueryAutor = 'SELECT u.idUsuario, u.nombre, u.email FROM ?? mf JOIN ?? f ON f.idForo = mf.foro JOIN ?? u ON u.idUsuario = f.propietario GROUP BY u.idUsuario ORDER BY count(mf.idMensaje_foro) DESC';
+        let queryAutor = mysql.format(selectQueryAutor,["mensaje_foro","foro","usuario"]);
+        pool.query(queryAutor,(err,dataAutores) => {
             if(err){
                 console.error(err);
                 throw error;
             }else{
-                res.render('trending',{
-                    login:true,
-                    id: req.session.idUsuario,
-                    foros:data
+                let selectQuery = 'SELECT f.idForo,f.propietario,f.nombre,f.descripcion,count(m.idMensaje_foro) mensajes FROM bocaillo.foro f' + 
+                ' left join mensaje_foro m ON f.idForo = m.foro group by f.idForo ORDER BY COUNT(m.idMensaje_foro) DESC';
+                let query = mysql.format(selectQuery,["foro"]);
+                pool.query(query,(err,data) => {
+                    if(err){
+                        console.error(err);
+                        throw error;
+                    }else{
+                        res.render('trending',{
+                            login:true,
+                            id: req.session.idUsuario,
+                            foros:data,
+                            autores:dataAutores
+                        });
+                    }
                 });
             }
-    });
-    }else{
+        });
+
+    } else{
         res.render('index',{
             login: false,
             name: 'Debe iniciar sesión'
         })
     }
+
 };
