bca4d2c93e2e73196061143b9ab9f2c9f417f5b0
nicor <nicoromero07@uma.es>
2022-05-31 11:58:43 +0200

    database data

--- database/bocailloData.sql
+++ database/bocailloData.sql
@@ -1,7 +1,13 @@
 INSERT INTO categoria(nombre) VALUES('Deporte');
+INSERT INTO categoria(nombre) VALUES('Lectura');
+INSERT INTO categoria(nombre) VALUES('Bricolaje');
+INSERT INTO categoria(nombre) VALUES('Tecnología');
+INSERT INTO categoria(nombre) VALUES('Cocina');
+
 
 INSERT INTO usuario(nombre,email,contrasenya) VALUES('Juan','juan@gmail.com','$2a$12$ZFnSvY1lN9Ed.tAvARqy9.OYJV8ERS3HrqeRTQne2cqBxARhxOLoa');
 INSERT INTO usuario(nombre,email,contrasenya) VALUES('Paco','paco@gmail.com','$2a$12$ZFnSvY1lN9Ed.tAvARqy9.OYJV8ERS3HrqeRTQne2cqBxARhxOLoa');
+INSERT INTO usuario(nombre,email,contrasenya) VALUES('NicoRomero07','nicoromero07@hotmail.com','$2a$12$ZFnSvY1lN9Ed.tAvARqy9.OYJV8ERS3HrqeRTQne2cqBxARhxOLoa');
 
-INSERT INTO foro(propietario,nombre,descripcion,categoria) VALUES(4,'JJOO2020','Foro de los juegos olimpicos de Tokio',1);
+INSERT INTO foro(propietario,nombre,descripcion,categoria) VALUES(1,'JJOO2020','Foro de los juegos olimpicos de Tokio',1);
 
