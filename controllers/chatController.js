914c4172e18be41036ed69afb137e12dd15014c0
pablogodiaz <pablogodiaz+1@gmail.com>
2022-06-06 17:47:49 +0200

    min-height chatView

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -41,11 +41,9 @@ exports.enviarMensaje = (req, res) => {
                 console.log(error);
             }else{
                 res.redirect('/vistaChat/' + idReceptorP);
-                window.scrollTo(0,document.body.scrollHeight);
             }
         });
     } else {
         res.redirect('/vistaChat/' + idReceptorP);
-        window.scrollTo(0,document.body.scrollHeight);
     }
 }
